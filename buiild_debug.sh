a0be0f164af323e59f01165a6a1f10c6b79f6c63
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-10-23 08:44:10 +0700

    add build shell fiiles for unix

---
+++ buiild_debug.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BUILD_DIR=$(dirname $0)/build
+SOURCE_DIR=$(dirname $0)/source
+BUILD_JS=$(dirname $0)/n0d3s/build.js
+RESOURCES_DIR=$(dirname $0)/resources
+
+if [ ! -d "$BUILD_DIR" ]; then
+    mkdir -p "$BUILD_DIR"
+fi
+
+node "$BUILD_JS" -s "$SOURCE_DIR" -b "$BUILD_DIR" -res "$RESOURCES_DIR" -jsem false -jsen false "$@"
