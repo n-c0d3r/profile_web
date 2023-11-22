216a7178da061ed1156c31380db7d79d888120c0
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2023-11-22 11:14:01 +0700

    update blog

--- source/blog/posts/1.introduction_to_3d_computer_graphics/info.js
+++ source/blog/posts/1.introduction_to_3d_computer_graphics/info.js
@@ -3,13 +3,13 @@ module;
 
 return {
 
-    title: "Introduction to Computer Graphics",
+    title: "Introduction to 3D Computer Graphics",
 
     author: "NCoder",
     date: "November 22th, 2023",
 
     thumbnailURL: `${window.origin}/resources/image/for_blogs/dx11.png`,
-    targetURL: `${window.origin}/build/pages/blog/posts/1.introduction_to_computer_graphics/page.html`,
+    targetURL: `${window.origin}/build/pages/blog/posts/1.introduction_to_3d_computer_graphics/page.html`,
 
     tags: [ 
 
