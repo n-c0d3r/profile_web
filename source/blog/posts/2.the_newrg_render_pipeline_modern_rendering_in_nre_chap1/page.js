a2f904da1361bbbadf3a0b65910b2db56516cf1a
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-09-03 00:00:36 +0700

    .

--- source/blog/posts/2.the_newrg_render_pipeline_modern_rendering_in_nre_chap1/page.js
+++ source/blog/posts/2.the_newrg_render_pipeline_modern_rendering_in_nre_chap1/page.js
@@ -10,8 +10,10 @@ module.use({
 
 })
 .text({
-
+    
     introduction: "text/blog/posts/2.the_newrg_render_pipeline_modern_rendering_in_nre/introduction",
+    differences_between_firstrp_and_newrg: "text/blog/posts/2.the_newrg_render_pipeline_modern_rendering_in_nre/differences_between_firstrp_and_newrg",
+    multithreaded_rendering: "text/blog/posts/2.the_newrg_render_pipeline_modern_rendering_in_nre/multithreaded_rendering",
     
 })
 .register_page();
@@ -20,7 +22,6 @@ module.use({
 
 Post(info)
 .appendInner(
-
     Post.Paragraph("Introduction"),
     Post.EnhancedTextSection(introduction),
     Post.ImageBody(
@@ -30,4 +31,17 @@ Post(info)
 
     Common.HR(),
 
+    Post.Paragraph("FirstRP vs NewRG"),
+    Post.EnhancedTextSection(differences_between_firstrp_and_newrg),
+
+    Common.HR(),
+
+    Post.Paragraph("Multithreaded Rendering"),
+    Post.EnhancedTextSection(multithreaded_rendering),
+    Post.ImageBody(
+        `${window.origin}/resources/image/for_blogs/nre_render_graph.png`,
+        "https://github.com/INGTechnologies/ING/blob/main/Source/Engine/ING/Source/ING/Engine/Engine.cpp"
+    ),
+
+    Common.HR(),
 );
